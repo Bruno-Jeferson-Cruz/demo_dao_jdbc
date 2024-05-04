fd3128a6da32ce27901d140714879f4d31e88132
Bruno-Jeferson-Cruz <bjefersoncruz@gmail.com>
2024-05-04 13:57:08 -0300

    SellerDaoJDC e Dao Factory

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,13 +2,15 @@ package model.dao;
 
 import java.util.List;
 
-public interface Seller {
+import model.entities.Seller;
+
+public interface SellerDao {
 
 	void insert(Seller obj);
 	void update(Seller obj);
 	void deleteById(Integer id);
 	Seller findById(Integer id);
-	List<Seller> findall();
+	List<SellerDao> findall();
 	
 	
 }
